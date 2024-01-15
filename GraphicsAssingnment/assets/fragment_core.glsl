51235ff10bce1509c31bba49f696e6b0be557fc4
uptheshipcreek <northenore@gmail.com>
2024-01-15 13:57:08 +0200

    Changes in the shaders

--- GraphicsAssingnment/assets/fragment_core.glsl
+++ GraphicsAssingnment/assets/fragment_core.glsl
@@ -1,13 +1,13 @@
 #version 330 core
 
-in vec3 fragPos; // Input vertex position
 out vec4 fragColor; // Output color
 
+in vec3 vertexColor; // Input from the vertex shader
+in vec2 TexCoord; // Input from the vertex shader
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
 void main() {
-    if (distance(fragPos, vec3(0.5f, 0.0f, 0.0f)) < 0.5 || distance(fragPos, vec3(0.5f, 0.5f, 0.0f)) < 0.5) {
-        fragColor = vec4(1.0, 0.0, 0.0, 1.0); // Red color if close to specified positions
-    } 
-    else {
-        fragColor = vec4(0.0, 0.0, 1.0, 1.0); // Green color otherwise
-    }
+    fragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 1);
 }
--- GraphicsAssingnment/assets/vertex_core.glsl
+++ GraphicsAssingnment/assets/vertex_core.glsl
@@ -1,9 +1,19 @@
 #version 330 core
 
-layout(location = 0) in vec3 aPos; // Assuming aPos is an attribute
-out vec3 fragPos; // Pass the position to the fragment shader
+layout(location = 0) in vec3 aPos; // Position
+layout(location = 1) in vec3 aColor; // Color
+layout(location = 2) in vec2 aTexCoord; // Texture
+
+out vec3 vertexColor; // Output a color to the fragment shader
+out vec2 TexCoord; // Output a texture coordinate to the fragment shader
+
+uniform mat4 transform;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
 
 void main() {
-    gl_Position = vec4(aPos, 1.0);
-    fragPos = aPos;
+	gl_Position = projection * view * model * transform * vec4(aPos, 1.0f);
+    vertexColor = aColor;
+	TexCoord = aTexCoord;
 }
