bb17f632dcbe3d8a39b9444234f39f347142cdaa
uptheshipcreek <northenore@gmail.com>
2024-01-22 20:10:24 +0200

    Shaders for the sun and the planets

--- GraphicsAssingnment/assets/fragment_light.glsl
+++ GraphicsAssingnment/assets/fragment_light.glsl
@@ -1,7 +1,15 @@
 #version 330 core
 out vec4 FragColor;
 
+in vec2 TexCoords;
+
+uniform sampler2D texture_diffuse1;
+
 void main()
-{
-    FragColor = vec4(1.0f); // set all 4 vector values to 1.0
+{    
+    vec3 ambient = 0.8 * vec3(1.0, 1.0, 1.0);
+    vec3 objectColor = texture(texture_diffuse1,  TexCoords).rgb;
+    vec3 result = objectColor * ambient;
+
+    FragColor = vec4(result, 1.0);
 }
--- GraphicsAssingnment/assets/fragment_model.glsl
+++ GraphicsAssingnment/assets/fragment_model.glsl
@@ -1,12 +1,34 @@
 #version 330 core
 out vec4 FragColor;
 
+in vec3 FragPos;  
+in vec3 Normal;  
 in vec2 TexCoords;
 
+struct Light {
+    vec3 position;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
 uniform sampler2D texture_diffuse1;
+uniform vec3 viewPos;
+uniform Light light;
+
 
 void main()
-{    
-    FragColor = texture(texture_diffuse1, TexCoords);
-}
-
+{
+    // ambient
+    vec3 ambient = light.ambient * texture(texture_diffuse1, TexCoords).rgb;
+  	
+    // diffuse 
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(light.position - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = light.diffuse * diff * texture(texture_diffuse1, TexCoords).rgb;  
+      
+    vec3 result = ambient + diffuse;
+    FragColor = vec4(result, 1.0);
+}   
